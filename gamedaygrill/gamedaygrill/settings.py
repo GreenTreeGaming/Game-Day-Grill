db8adecf83b383841d271983b41c177eaec9322f
Sarvajith Karun <obbyslicedyt@gmail.com>
2024-01-03 21:04:54 -0600

    debug off

--- gamedaygrill/gamedaygrill/settings.py
+++ gamedaygrill/gamedaygrill/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d%+m!$h2)7)*d@kn_*nma_d()i*mk4^=q^7zp0((td(q2()v&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
